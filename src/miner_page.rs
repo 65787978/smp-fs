0cabd251874739df0fa1989bdbcd717c22a10c00
ergXned <126957013+ergXned@users.noreply.github.com>
2024-07-25 16:17:11 +0200

    minor update to miner_page

--- src/miner_page.rs
+++ src/miner_page.rs
@@ -1,14 +1,25 @@
-use crate::utils::*;
+use crate::{data::api_data::get_data, utils::*};
 use dioxus::prelude::*;
 
 pub fn MinerPage_slice(address: String) -> Element {
-    rsx! {
+    let address = use_signal(|| String::from(address));
+    let data = use_server_future(move || async move { get_data(address()).await })?;
 
-            div {class:"grid sm:grid-cols-3 gap-2 m-2",
-                {Card("9", "bg-blue-300", address)}
-                {InfoCardDouble("8", "bg-gray-300 m-4 text-center", "19.04", "Th/s", "Network Hashrate", "86.55", "Gh/s", "Pool Hashrate")}
-                {InfoCardDouble("8", "bg-gray-300 m-4 text-center", "19.04", "Th/s", "Network Hashrate", "86.55", "Gh/s", "Pool Hashrate")}
-                // {Card("9", "bg-gray-300", "04".to_string())}
+    match &*data.read_unchecked() {
+        Some(Ok(stats)) => {
+            rsx! {
+                    div {class:"grid sm:grid-cols-3 gap-2 m-2",
+                        {Card("9", "bg-blue-300", address())}
+                        {InfoCardDouble("8", "bg-gray-300 m-4 text-center", stats.pool.effort.to_string().as_str(), "%", "Pool Effort", stats.miner.paid_24h.to_string().as_str(), " ERG", "24h Paid")}
+                        {InfoCardDouble("8", "bg-gray-300 m-4 text-center", stats.miner.hashrate_current.to_string().as_str(), "Mh/s", "Miner Hashrate", stats.miner.workers_number.to_string().as_str(), "", "Active Workers")}
+                    }
             }
+        }
+        Some(Err(error)) => {
+            rsx!()
+        }
+        None => {
+            rsx!()
+        }
     }
 }
