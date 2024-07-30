2daac6908a68c14b0d4de82809872ef2333e5048
ergXned <126957013+ergXned@users.noreply.github.com>
2024-07-30 13:17:29 +0200

    refactor

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -7,6 +7,24 @@ pub fn HomePage_slice() -> Element {
     let mut data =
         use_server_future(move || async move { get_home_page_data("".to_string()).await })?;
 
+    rsx!(
+            div {class:"grid sm:grid-cols-1 justify-items-center",
+            div { class:"m-3",
+                img {max_width:"15rem", max_height:"15rem",src: "sig-logo.png"}
+            }
+
+            div {class:"font-mono text-2xl text-slate-200 font-bold", "SIGMANAUTS"}
+
+            div {class:"text-lg text-center text-slate-200 underline decoration-orange-500", "A community to empower users of the Ergo blockchain"}
+
+            {ParagraphCard(utils::ParagraphCardProps { vars: ParagraphCard {
+
+                classes: "bg-inherit m-4 text-center text-slate-300 text-xl".to_string(),
+                text: "Welcome to the Sigmanauts pool, a DAO-driven, community-run mining pool dedicated to supporting the Ergo ecosystem. Joining us not only contributes to the Ergo community (fees go to Sigmanauts treasury) but also offers hourly bonus token payments".to_string(),
+            }})}
+
+        }
+    );
     match &*data.read_unchecked() {
         Some(Ok(stats)) => {
             rsx! {
