b43bbdc5604a5d26c6f9a0afc4aa5ea88fa054eb
ergXned <126957013+ergXned@users.noreply.github.com>
2024-07-30 13:24:40 +0200

    clean up

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -3,113 +3,112 @@ use crate::utils::{self, *};
 use dioxus::prelude::*;
 
 pub fn HomePage_slice() -> Element {
-    // let mut data = use_server_future(move || get_data());
     let mut data =
         use_server_future(move || async move { get_home_page_data("".to_string()).await })?;
 
-    rsx!(
+    rsx! {
             div {class:"grid sm:grid-cols-1 justify-items-center",
-            div { class:"m-3",
-                img {max_width:"15rem", max_height:"15rem",src: "sig-logo.png"}
+                div { class:"m-3",
+                    img {max_width:"15rem", max_height:"15rem",src: "sig-logo.png"}
+                }
+
+                div {class:"font-mono text-2xl text-slate-200 font-bold", "SIGMANAUTS"}
+
+                div {class:"text-lg text-center text-slate-200 underline decoration-orange-500", "A community to empower users of the Ergo blockchain"}
+
+                {ParagraphCard(utils::ParagraphCardProps { vars: ParagraphCard {
+
+                    classes: "bg-inherit m-4 text-center text-slate-300 text-xl".to_string(),
+                    text: "Welcome to the Sigmanauts pool, a DAO-driven, community-run mining pool dedicated to supporting the Ergo ecosystem. Joining us not only contributes to the Ergo community (fees go to Sigmanauts treasury) but also offers hourly bonus token payments".to_string(),
+                }})}
+
             }
 
-            div {class:"font-mono text-2xl text-slate-200 font-bold", "SIGMANAUTS"}
+        match &*data.read_unchecked() {
+            Some(Ok(stats)) => {
+                rsx! {
+                    div {class:"grid sm:grid-cols-1 justify-items-center",
+                        div { class:"m-3",
+                            img {max_width:"15rem", max_height:"15rem",src: "sig-logo.png"}
+                        }
 
-            div {class:"text-lg text-center text-slate-200 underline decoration-orange-500", "A community to empower users of the Ergo blockchain"}
+                        div {class:"font-mono text-2xl text-slate-200 font-bold", "SIGMANAUTS"}
 
-            {ParagraphCard(utils::ParagraphCardProps { vars: ParagraphCard {
+                        div {class:"text-lg text-center text-slate-200 underline decoration-orange-500", "A community to empower users of the Ergo blockchain"}
 
-                classes: "bg-inherit m-4 text-center text-slate-300 text-xl".to_string(),
-                text: "Welcome to the Sigmanauts pool, a DAO-driven, community-run mining pool dedicated to supporting the Ergo ecosystem. Joining us not only contributes to the Ergo community (fees go to Sigmanauts treasury) but also offers hourly bonus token payments".to_string(),
-            }})}
+                        {ParagraphCard(utils::ParagraphCardProps { vars: ParagraphCard {
 
-        }
-    );
-    match &*data.read_unchecked() {
-        Some(Ok(stats)) => {
-            rsx! {
-                div {class:"grid sm:grid-cols-1 justify-items-center",
-                    div { class:"m-3",
-                        img {max_width:"15rem", max_height:"15rem",src: "sig-logo.png"}
+                            classes: "bg-inherit m-4 text-center text-slate-300 text-xl".to_string(),
+                            text: "Welcome to the Sigmanauts pool, a DAO-driven, community-run mining pool dedicated to supporting the Ergo ecosystem. Joining us not only contributes to the Ergo community (fees go to Sigmanauts treasury) but also offers hourly bonus token payments".to_string(),
+                        }})}
+
                     }
 
-                    div {class:"font-mono text-2xl text-slate-200 font-bold", "SIGMANAUTS"}
+                    div {class:"grid sm:grid-cols-4",
 
-                    div {class:"text-lg text-center text-slate-200 underline decoration-orange-500", "A community to empower users of the Ergo blockchain"}
+                        {InfoCard(utils::InfoCardProps { vars: InfoCard {
+                            classes: "".to_string(),
+                            value: stats.network.hashrate.to_string(),
+                            unit: "Th/s".to_string(),
+                            heading: "Network Hashrate".to_string()
 
-                    {ParagraphCard(utils::ParagraphCardProps { vars: ParagraphCard {
+                        } })}
 
-                        classes: "bg-inherit m-4 text-center text-slate-300 text-xl".to_string(),
-                        text: "Welcome to the Sigmanauts pool, a DAO-driven, community-run mining pool dedicated to supporting the Ergo ecosystem. Joining us not only contributes to the Ergo community (fees go to Sigmanauts treasury) but also offers hourly bonus token payments".to_string(),
-                    }})}
+                        {InfoCard(utils::InfoCardProps { vars: InfoCard {
+                            classes: "".to_string(),
+                            value: stats.network.height.to_string(),
+                            unit: "".to_string(),
+                            heading: "Network Height".to_string()
 
-                }
+                        } })}
 
-                div {class:"grid sm:grid-cols-4",
+                        {InfoCard(utils::InfoCardProps { vars: InfoCard {
+                            classes: "".to_string(),
+                            value: stats.pool.hashrate.to_string(),
+                            unit: "Gh/s".to_string(),
+                            heading: "Pool Hashrate".to_string()
 
-                    {InfoCard(utils::InfoCardProps { vars: InfoCard {
-                        classes: "".to_string(),
-                        value: stats.network.hashrate.to_string(),
-                        unit: "Th/s".to_string(),
-                        heading: "Network Hashrate".to_string()
+                        } })}
 
-                    } })}
+                        {InfoCard(utils::InfoCardProps { vars: InfoCard {
+                            classes: "".to_string(),
+                            value: stats.pool.connected_miners.to_string(),
+                            unit: "".to_string(),
+                            heading: "Pool Miners".to_string()
 
-                    {InfoCard(utils::InfoCardProps { vars: InfoCard {
-                        classes: "".to_string(),
-                        value: stats.network.height.to_string(),
-                        unit: "".to_string(),
-                        heading: "Network Height".to_string()
+                        } })}
+                    }
 
-                    } })}
+                    div {class:"grid sm:grid-cols-2",
 
-                    {InfoCard(utils::InfoCardProps { vars: InfoCard {
-                        classes: "".to_string(),
-                        value: stats.pool.hashrate.to_string(),
-                        unit: "Gh/s".to_string(),
-                        heading: "Pool Hashrate".to_string()
+                        {InfoCard(utils::InfoCardProps { vars: InfoCard {
+                            classes: "".to_string(),
+                            value: "pool.ergo-sig-mining.net:3053".to_string(),
+                            unit: "".to_string(),
+                            heading: "URL under 10 Gh/s".to_string()
 
-                    } })}
-
-                    {InfoCard(utils::InfoCardProps { vars: InfoCard {
-                        classes: "".to_string(),
-                        value: stats.pool.connected_miners.to_string(),
-                        unit: "".to_string(),
-                        heading: "Pool Miners".to_string()
-
-                    } })}
-                }
-
-                div {class:"grid sm:grid-cols-2",
-
-                    {InfoCard(utils::InfoCardProps { vars: InfoCard {
-                        classes: "".to_string(),
-                        value: "pool.ergo-sig-mining.net:3053".to_string(),
-                        unit: "".to_string(),
-                        heading: "URL under 10 Gh/s".to_string()
-
-                    } })}
+                        } })}
 
 
-                    {InfoCard(utils::InfoCardProps { vars: InfoCard {
-                        classes: "".to_string(),
-                        value: "pool.ergo-sig-mining.net:3055".to_string(),
-                        unit: "".to_string(),
-                        heading: "URL over 10 Gh/s".to_string()
+                        {InfoCard(utils::InfoCardProps { vars: InfoCard {
+                            classes: "".to_string(),
+                            value: "pool.ergo-sig-mining.net:3055".to_string(),
+                            unit: "".to_string(),
+                            heading: "URL over 10 Gh/s".to_string()
 
-                    } })}
+                        } })}
+
+                    }
 
                 }
-
-
             }
-        }
-        Some(Err(err)) => {
-            rsx! { h1 {"{err}"}}
-        }
-        None => {
-            rsx! {
-                    h1 {"Loading..."}
+            Some(Err(err)) => {
+                rsx! { h1 {"{err}"}}
+            }
+            None => {
+                rsx! {
+                        h1 {"Loading..."}
+                }
             }
         }
     }
